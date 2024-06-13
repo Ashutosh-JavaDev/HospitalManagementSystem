f57cc50e2c48adcfb26592dd3a7855a8de22ea91
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-13 22:51:18 +0530

    Hospiital Management System

--- src/Login.java
+++ src/Login.java
@@ -5,11 +5,12 @@ import java.awt.Image;
 import java.awt.Color;
 
 public class Login extends JFrame implements ActionListener {
+    JButton New;
     Login() {
 
     }
     public void actionPerformed(ActionEvent ae){
-        
+
     }
     public static void main(String[] args) throws Exception {
         new Login();
