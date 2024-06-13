4a4e0d7738fad4da2dd0f861e23a28f55d50d4ea
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-13 22:49:25 +0530

    Hospiital Management System

--- src/Login.java
+++ src/Login.java
@@ -3,8 +3,15 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Image;
 import java.awt.Color;
-public class Login {
+
+public class Login extends JFrame implements ActionListener {
+    Login() {
+
+    }
+    public void actionPerformed(ActionEvent ae){
+        
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new Login();
     }
 }
