2e77e54d50b3793765c78aac09df6eb9a51fbf81
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-14 21:54:47 +0530

    Hospiital Management System

--- src/Login.java
+++ src/Login.java
@@ -4,29 +4,30 @@ import java.awt.event.ActionEvent;
 import java.awt.Image;
 import java.awt.Color;
 import java.awt.Font;
+
 public class Login extends JFrame implements ActionListener {
     JButton Login, Forget;
     JTextField loginfield, passwordfield;
 
     Login() {
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-jmeyer1220-668300.jpg"));
-        Image i2=i1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
-        ImageIcon i3=new ImageIcon(i2);
-        JLabel image=new JLabel(i3);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/pexels-jmeyer1220-668300.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
         image.setBounds(0, 0, 1000, 700);
         add(image);
-        JLabel username=new JLabel("UserName");
-        username.setBounds(400, 300,100,30);
-        username.setFont(new Font("Arial",Font.BOLD,18));
+        JLabel username = new JLabel("UserName");
+        username.setBounds(400, 300, 100, 30);
+        username.setFont(new Font("Arial", Font.BOLD, 18));
         add(username);
-        JPasswordField password=new JPasswordField("Password");
-        password.setBounds(400, 350,100,30);
-        password.setFont(new Font("Arial",Font.BOLD,18));
+        JPasswordField password = new JPasswordField("Password");
+        password.setBounds(400, 350, 100, 30);
+        password.setFont(new Font("Arial", Font.BOLD, 18));
         add(password);
         // Default
         setTitle("Hospital Management System");
         setLayout(null);
-        setSize(1000,700);
+        setSize(1000, 700);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(400, 150);
         setVisible(true);
