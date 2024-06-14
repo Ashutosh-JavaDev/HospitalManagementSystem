75f5d0f23e5f16269219ed70cccd4615c5a79edc
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-14 21:30:42 +0530

    Hospiital Management System

--- src/Login.java
+++ src/Login.java
@@ -5,18 +5,21 @@ import java.awt.Image;
 import java.awt.Color;
 
 public class Login extends JFrame implements ActionListener {
-    JButton Login,Forget;
-    JTextField loginfield,passwordfield;
+    JButton Login, Forget;
+    JTextField loginfield, passwordfield;
+
     Login() {
         setTitle("Hospital Management System");
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocation(200,150);
+        setLocation(200, 150);
         setVisible(true);
     }
-    public void actionPerformed(ActionEvent ae){
+
+    public void actionPerformed(ActionEvent ae) {
 
     }
+
     public static void main(String[] args) throws Exception {
         new Login();
     }
