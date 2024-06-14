89d406388daa0a1885affa6dcfba505bd94327f0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-14 22:02:44 +0530

    Hospiital Management System

--- src/Login.java
+++ src/Login.java
@@ -11,10 +11,10 @@ public class Login extends JFrame implements ActionListener {
 
     Login() {
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/pexels-jmeyer1220-668300.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(1000, 800, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
-        image.setBounds(0, 0, 1000, 700);
+        image.setBounds(0, 0, 1000, 800);
         add(image);
         JLabel username = new JLabel("UserName");
         username.setBounds(300, 300, 100, 30);
@@ -30,11 +30,19 @@ public class Login extends JFrame implements ActionListener {
         passwordfield=new JTextField();
         passwordfield.setBounds(500,350,150,30);
         add(passwordfield);
-
+        // Button
+        Login=new JButton("Login");
+        Login.setBounds(300,410,100,30);
+        Login.setFont(new Font("Arial", Font.BOLD, 14));
+        add(Login);
+        Forget=new JButton("Forget Password");
+        Forget.setBounds(500,410,200,30);
+        Forget.setFont(new Font("Arial", Font.BOLD, 14));
+        add(Forget);
         // Default
         setTitle("Hospital Management System");
         setLayout(null);
-        setSize(1000, 700);
+        setSize(1000, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(400, 150);
         setVisible(true);
