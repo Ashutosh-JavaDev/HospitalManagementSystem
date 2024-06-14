3ea17c90cfc9cdfc0f07b7f48b93441205b8cb45
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-14 21:34:20 +0530

    Hospiital Management System

--- src/Login.java
+++ src/Login.java
@@ -12,7 +12,8 @@ public class Login extends JFrame implements ActionListener {
         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-jmeyer1220-668300.jpg"));
         Image i2=i1.getImage().getScaledInstance(150, 300, Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
-        JLabel image=new JLabel();
+        JLabel image=new JLabel(i3);
+        add(image);
         // Default
         setTitle("Hospital Management System");
         setLayout(null);
