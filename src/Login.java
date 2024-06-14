6fc7cd60fd0f0cb2e17c298432b0bfcc6491c347
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-14 21:30:33 +0530

    Hospiital Management System

--- src/Login.java
+++ src/Login.java
@@ -8,7 +8,11 @@ public class Login extends JFrame implements ActionListener {
     JButton Login,Forget;
     JTextField loginfield,passwordfield;
     Login() {
-
+        setTitle("Hospital Management System");
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(200,150);
+        setVisible(true);
     }
     public void actionPerformed(ActionEvent ae){
 
