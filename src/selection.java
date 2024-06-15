48790fbae1ff71ee21ad212694726c94ffe2a550
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-15 20:32:44 +0530

    Hospiital Management System

--- src/selection.java
+++ src/selection.java
@@ -3,7 +3,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Image;
 import java.awt.Color;
-import java.awt.Font
-;;public class selection {
-    
+import java.awt.Font;
+public class selection extends JFrame implements ActionListener {
+    selection(){
+
+    }
+    public void actionPerformed(ActionEvent ae){
+
+    }
+    public static void main(String[]args){
+        new selection();
+    }
 }
