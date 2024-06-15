52c3a5506fa6874831e872e8eeb6abd7b07c26be
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-15 20:39:28 +0530

    Hospiital Management System

--- src/selection.java
+++ src/selection.java
@@ -7,14 +7,14 @@ import java.awt.Font;
 public class selection extends JFrame implements ActionListener {
     selection(){
         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-jmeyer1220-668300.jpg"));
-        Image i2=i1.getImage().getScaledInstance(500, 400, Image.SCALE_DEFAULT);
+        Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
         JLabel image=new JLabel(i3);
-        image.setBounds(0, 0, 500, 400);
+        image.setBounds(0, 0, 800, 600);
         add(image);
         // default
         setTitle("Hospital Management System");
-        setSize(500,400);
+        setSize(800,600);
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
