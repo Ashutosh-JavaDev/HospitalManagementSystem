0d9af38c5e4128544a277ad78458ad3c43920f28
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-16 10:26:11 +0530

    Hospiital Management System

--- src/Patient.java
+++ src/Patient.java
@@ -7,13 +7,18 @@ import java.awt.Image;
 public class Patient extends JFrame implements ActionListener{
     JLabel name,ID,phone,email,room,cause,date,gaurdian;
     Patient(){
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-jmeyer1220-668300.jpg"));
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/.jgp"));
         Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
         JLabel image=new JLabel(i3);
         image.setBounds(0,0,800,600);
         add(image);
-
+        //Name 
+        name=new JLabel("Patient's Name: ");
+        name.setFont(new Font("Arial",Font.BOLD,18));
+        name.setForeground(Color.white);
+        name.setBounds(100,25,200,20);
+        image.add(name);
         // Default
         setTitle("Patient Details");
         setLayout(null);
