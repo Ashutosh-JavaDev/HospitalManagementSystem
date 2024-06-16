0e2d57a1e76cf11e1497bc6d7782014e6e020938
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-16 10:34:55 +0530

    Hospiital Management System

--- src/Patient.java
+++ src/Patient.java
@@ -8,7 +8,7 @@ public class Patient extends JFrame implements ActionListener{
     JLabel name,ID,phone,email,room,cause,date,gaurdian;
     JTextField namefield,idfield,emailfield,phonefield,roomfield,causefield,datefield,gaurdianfield;
     Patient(){
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/.jgp"));
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
         Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
         JLabel image=new JLabel(i3);
@@ -16,13 +16,13 @@ public class Patient extends JFrame implements ActionListener{
         add(image);
         //Name 
         name=new JLabel("Patient's Name: ");
-        name.setFont(new Font("Arial",Font.BOLD,18));
-        name.setForeground(Color.BLACK);
-        name.setBounds(100,25,200,20);
+        name.setFont(new Font("Arial",Font.BOLD,20));
+        name.setForeground(Color.white);
+        name.setBounds(100,35,200,30);
         image.add(name);
         namefield=new JTextField();
-        namefield.setFont(new Font("Arial",Font.PLAIN,16));
-        namefield.setBounds(350,25,200,20);
+        namefield.setFont(new Font("Arial",Font.PLAIN,18));
+        namefield.setBounds(350,35,200,30);
         image.add(namefield);
         // Default
         setTitle("Patient Details");
