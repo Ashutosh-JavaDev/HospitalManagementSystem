00007ef41e2e54f09738fa9487485958a6f0af0b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-16 10:07:27 +0530

    Hospiital Management System

--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,9 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
 public class Patient {
     
 }
