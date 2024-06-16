9ec4843e48c65c94dd67604507792f1dd145335f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-16 10:10:55 +0530

    Hospiital Management System

--- src/Patient.java
+++ src/Patient.java
@@ -6,9 +6,14 @@ import java.awt.Font;
 import java.awt.Image;
 public class Patient extends JFrame implements ActionListener{
     Patient(){
-
+        ImageIcon i1=new ImageIcon("Images/pexels-jmeyer1220-668300.jpg");
+        Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
+        image.setBounds(0,0,800,600);
+        add(image);
     }
     public void actionPerformed(ActionEvent ae){
-        
+
     }
 }
