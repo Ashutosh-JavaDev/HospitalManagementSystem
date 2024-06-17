d15f3fccaa764b666c26f3e55858daa9cdada41c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-17 13:27:06 +0530

    Hospiital Management System

--- src/HospitalManagementSystem.java
+++ src/HospitalManagementSystem.java
@@ -2,7 +2,16 @@ import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
+
+import javax.swing.JOptionPane;
 public class HospitalManagementSystem {
     Connection conn;
     Statement state;
+    public HospitalManagementSystem()throws SQLException{
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(ClassNotFoundException ae){
+            JOptionPane.showMessageDialog(null, ae, "Class Not Found", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
