e46f1ee0be43a4d5b4e37a7bbd132b2826ea6e87
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-18 15:30:14 +0530

    Hospiital Management System

--- src/Employee.java
+++ src/Employee.java
@@ -6,7 +6,7 @@ import java.awt.Color;
 import java.awt.Font;
 public class Employee extends JFrame implements ActionListener{
     public Employee(){
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Image/pexels-scottwebb-311458.jpg"));
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
         Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
         JLabel image=new JLabel(i3);
