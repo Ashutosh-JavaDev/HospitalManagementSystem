d5dd6639f4601304182f604b44100c98071e0924
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-18 15:17:17 +0530

    Hospiital Management System

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Image;
+import java.awt.Color;
+import java.awt.Font;
 public class Hospital {
     
 }
