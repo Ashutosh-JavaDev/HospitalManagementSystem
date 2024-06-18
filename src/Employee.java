59a7f8155bb7f60dbfbc254a93662745b515e71f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-18 15:35:42 +0530

    Hospiital Management System

--- src/Employee.java
+++ src/Employee.java
@@ -1,33 +1,42 @@
 import javax.swing.*;
+
+import com.toedter.calendar.JDateChooser;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Image;
 import java.awt.Color;
 import java.awt.Font;
-public class Employee extends JFrame implements ActionListener{
-    JLabel name,proffesion,ID,email,phone,IDnumber,doj,maritial;
-    public Employee(){
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
-        Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
-        ImageIcon i3=new ImageIcon(i2);
-        JLabel image=new JLabel(i3);
-        image.setBounds(0,0,800,600);
+
+public class Employee extends JFrame implements ActionListener {
+    JLabel name, proffesion, ID, email, phone, IDnumber, doj, maritial;
+    JTextField namefield, idfield, emailfield, phonefield, idnumberfield, statusfield;
+    JComboBox proffesionfield;
+    JDateChooser dojfield;
+
+    public Employee() {
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 800, 600);
         add(image);
         // Name
         // Default Information
         setLayout(null);
         setTitle("Employee Entry");
-        setSize(800,600);
-        setLocation(250,150);
+        setSize(800, 600);
+        setLocation(250, 150);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
 
+    }
+
+    public void actionPerformed(ActionEvent ae) {
 
     }
-    public void actionPerformed(ActionEvent ae){
 
-    }
-    public static void main(String[]args){
+    public static void main(String[] args) {
         new Employee();
     }
 }
