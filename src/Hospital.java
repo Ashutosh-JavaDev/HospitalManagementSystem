f82eb2f738f9a012e31082eb04328dbbf5320ffd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-18 15:18:20 +0530

    Hospiital Management System

--- src/Hospital.java
+++ src/Hospital.java
@@ -4,6 +4,14 @@ import java.awt.event.ActionListener;
 import java.awt.Image;
 import java.awt.Color;
 import java.awt.Font;
-public class Hospital {
-    
+public class Hospital extends JFrame implements ActionListener{
+    public Hospital(){
+
+    }
+    public void actionPerformed(ActionEvent ae){
+
+    }
+    public static void main(String[]args){
+        new Hospital();
+    }
 }
