a387e1b8754d21ec300195446f7e4d5d3a633fa0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-18 15:28:07 +0530

    Hospiital Management System

--- src/Hospital.java
+++ src/Hospital.java
@@ -6,7 +6,13 @@ import java.awt.Color;
 import java.awt.Font;
 public class Hospital extends JFrame implements ActionListener{
     public Hospital(){
-
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Image/pexels-scottwebb-311458.jpg"));
+        Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
+        image.setBounds(0,0,800,600);
+        add(image);
+        
     }
     public void actionPerformed(ActionEvent ae){
 
