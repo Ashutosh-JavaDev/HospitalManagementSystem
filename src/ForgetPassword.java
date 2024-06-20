c65a7bdc40bf1ddfcc8ba01c3f2d4aa57ce09fb7
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-20 22:18:58 +0530

    Hospiital Management System

--- src/ForgetPassword.java
+++ src/ForgetPassword.java
@@ -6,8 +6,8 @@ import java.awt.Font;
 import java.awt.event.ActionListener;
 
 public class ForgetPassword extends JFrame implements ActionListener {
-    JLabel newpin, phonenumber,email;
-    JTextField oldpinfield, phonenumberfield,emailfield;
+    JLabel newpin, phonenumber, email;
+    JTextField oldpinfield, phonenumberfield, emailfield;
 
     public ForgetPassword() {
         ImageIcon i1 = new ImageIcon("pexels-scottwebb-311458.jpg");
@@ -27,17 +27,17 @@ public class ForgetPassword extends JFrame implements ActionListener {
         phonenumberfield.setBounds(310, 200, 150, 30);
         phonenumberfield.setFont(new Font("Raleway", Font.BOLD, 18));
         image.add(phonenumberfield);
-    // Forget pin
-    phonenumber = new JLabel("Enter Your Phone Number:");
-    phonenumber.setBounds(25, 200, 250, 30);
-    phonenumber.setFont(new Font("Raleway", Font.PLAIN, 16));
-    phonenumber.setForeground(Color.black);
-    image.add(phonenumber);
-    // Textfield
-    phonenumberfield = new JTextField();
-    phonenumberfield.setBounds(310, 200, 150, 30);
-    phonenumberfield.setFont(new Font("Raleway", Font.BOLD, 18));
-    image.add(phonenumberfield);
+        // Forget pin
+        email = new JLabel("Enter Your Phone Number:");
+        email.setBounds(25, 200, 250, 30);
+        email.setFont(new Font("Raleway", Font.PLAIN, 16));
+        email.setForeground(Color.black);
+        image.add(email);
+        // Textfield
+        emailfield = new JTextField();
+        emailfield.setBounds(310, 200, 150, 30);
+        emailfield.setFont(new Font("Raleway", Font.BOLD, 18));
+        image.add(emailfield);
         // Default
         setLayout(null);
         setSize(800, 600);
