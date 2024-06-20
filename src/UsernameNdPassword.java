f9f6047f782cb0324c987c5cfcbd4af9a0bf1b59
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-20 13:01:49 +0530

    Hospiital Management System

--- src/UsernameNdPassword.java
+++ src/UsernameNdPassword.java
@@ -7,41 +7,43 @@ import java.awt.event.ActionEvent;
 
 public class UsernameNdPassword extends JFrame implements ActionListener {
     JLabel username, password;
-    JButton save,back;
+    JButton save, back;
     JTextField usernamefield;
     JPasswordField passwordfield;
 
     public UsernameNdPassword() {
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
-        Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
-        ImageIcon i3=new ImageIcon(i2);
-        JLabel image=new JLabel(i3);
-        image.setBounds(0,0,800,600);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 800, 600);
         add(image);
         // Username
-        username=new JLabel("UserName:");
-        username.setBounds(25,200,100,30);
+        username = new JLabel("UserName:");
+        username.setBounds(25, 200, 100, 30);
         username.setForeground(Color.black);
-        username.setFont(new Font("Arial",Font.BOLD,20));
+        username.setFont(new Font("Arial", Font.BOLD, 20));
         image.add(username);
         // username Textfield
-        usernamefield=new JTextField();
-        usernamefield.setBounds(150,200,100,30);
+        usernamefield = new JTextField();
+        usernamefield.setBounds(150, 200, 100, 30);
         image.add(usernamefield);
-          // Password
-          password=new JLabel("Password:");
-          password.setBounds(25,250,100,30);
-          password.setForeground(Color.black);
-          password.setFont(new Font("Arial",Font.BOLD,20));
-          image.add(password);
-          // password Textfield
-          passwordfield=new JPasswordField();
-          passwordfield.setBounds(150,250,100,30);
-          image.add(passwordfield);
+        // Password
+        password = new JLabel("Password:");
+        password.setBounds(25, 250, 100, 30);
+        password.setForeground(Color.black);
+        password.setFont(new Font("Arial", Font.BOLD, 20));
+        image.add(password);
+        // password Textfield
+        passwordfield = new JPasswordField();
+        passwordfield.setBounds(150, 250, 100, 30);
+        image.add(passwordfield);
+        // Button
+
         // Default
         setLayout(null);
-        setSize(800,600);
-        setLocation(250,150);
+        setSize(800, 600);
+        setLocation(250, 150);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setUndecorated(true);
         setVisible(true);
