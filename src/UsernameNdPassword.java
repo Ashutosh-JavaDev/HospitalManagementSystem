47ceb5f04ac04093363ba406d5866000b1c546d0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-20 12:46:16 +0530

    Hospiital Management System

---
+++ src/UsernameNdPassword.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+public class UsernameNdPassword {
+    
+}
