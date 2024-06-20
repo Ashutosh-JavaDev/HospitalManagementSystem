e03af59ceacb6aac474fc3e84cb74295e119cbfb
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-20 12:53:10 +0530

    Hospiital Management System

--- src/UsernameNdPassword.java
+++ src/UsernameNdPassword.java
@@ -4,18 +4,22 @@ import java.awt.Color;
 import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-public class UsernameNdPassword extends JFrame implements ActionListener{
-    JLabel username,password;
+
+public class UsernameNdPassword extends JFrame implements ActionListener {
+    JLabel username, password;
     JButton save;
     JTextField usernamefield;
     JPasswordField passwordfield;
-    UsernameNdPassword(){
 
-    }   
-    public void actionPerformed(ActionEvent ae){
+    public UsernameNdPassword() {
 
     }
-    public static void main(String[]args){
+
+    public void actionPerformed(ActionEvent ae) {
+
+    }
+
+    public static void main(String[] args) {
         new UsernameNdPassword();
     }
 }
