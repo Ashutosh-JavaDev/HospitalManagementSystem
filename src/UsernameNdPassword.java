3c534990f6b7b3aa4d83d5ace1155cefdda15012
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-20 12:55:24 +0530

    Hospiital Management System

--- src/UsernameNdPassword.java
+++ src/UsernameNdPassword.java
@@ -12,7 +12,10 @@ public class UsernameNdPassword extends JFrame implements ActionListener {
     JPasswordField passwordfield;
 
     public UsernameNdPassword() {
-
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
+        Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
     }
 
     public void actionPerformed(ActionEvent ae) {
