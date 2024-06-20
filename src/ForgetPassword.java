69dca7241087c56f67006adfa44e7ea35f781924
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-20 21:43:40 +0530

    Hospiital Management System

--- src/ForgetPassword.java
+++ src/ForgetPassword.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class ForgetPassword {
-    
+public class ForgetPassword extends JFrame implements ActionListener {
+    public ForgetPassword(){
+
+    }
+    public void actionPerformed(ActionEvent ae){
+        
+    }
 }
