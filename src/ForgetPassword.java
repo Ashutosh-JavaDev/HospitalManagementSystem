0bf4b7dc6c269e593260b6dac1903a8c24bc7536
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-20 21:49:49 +0530

    Hospiital Management System

--- src/ForgetPassword.java
+++ src/ForgetPassword.java
@@ -1,11 +1,21 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Font;
 import java.awt.event.ActionListener;
 public class ForgetPassword extends JFrame implements ActionListener {
     public ForgetPassword(){
-
+        ImageIcon i1=new ImageIcon("pexels-scottwebb-311458.jpg");
+        Image i2=i1.getImage().getScaledInstance(600, 600, Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
+        image.setBounds(0,0,,800,600);
+        add(image);
+        // Default
+        
     }
     public void actionPerformed(ActionEvent ae){
-        
+
     }
 }
