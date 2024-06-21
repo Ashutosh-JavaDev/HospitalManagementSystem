6836d76d86a702ac225971c0a39567b3f08ff671
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-21 15:35:21 +0530

    Hospiital Management System

--- src/Appointment.java
+++ src/Appointment.java
@@ -4,6 +4,10 @@ import java.awt.event.ActionEvent;
 import java.sql.SQLException;
 public class Appointment extends JFrame implements ActionListener {
     public Appointment(){
+        //Default
+        setLayout(null);
+        setSize(800,600);
+        setLocation(250,150);
 
     }
     public void actionPerformed(ActionEvent ae){
