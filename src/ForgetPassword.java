4f93d19745c559054f80b696ac4704803b88b94f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-21 15:40:26 +0530

    Hospiital Management System

--- src/ForgetPassword.java
+++ src/ForgetPassword.java
@@ -80,6 +80,9 @@ public class ForgetPassword extends JFrame implements ActionListener {
         if(ae.getSource()==back){
             System.exit(0);
         }
+        else if(ae.getSource()==confirm){
+            
+        }
     }
 
     public static void main(String[] args) {
