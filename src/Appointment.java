997f34a83f216f3160e52f5282c946bc225c68d9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-22 14:52:26 +0530

    Hospital Management System

--- src/Appointment.java
+++ src/Appointment.java
@@ -6,6 +6,8 @@ import java.awt.Font;
 import java.awt.Color;
 import java.sql.SQLException;
 public class Appointment extends JFrame implements ActionListener {
+    JLabel name,phone,timing,cause,payment;
+    JTextField namefiekd,phonefield,timingfield,causefield,paymentfield;
     public Appointment(){
         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
         Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
@@ -13,6 +15,7 @@ public class Appointment extends JFrame implements ActionListener {
         JLabel image=new JLabel(i3);
         image.setBounds(0,0,800,600);
         add(image);
+
         //Default
         setLayout(null);
         setSize(800,600);
