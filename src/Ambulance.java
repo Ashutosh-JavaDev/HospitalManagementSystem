afbbf816235c3143099742ebb3aab0b633f3f5b2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-23 22:47:10 +0530

    Hospital Management System

--- src/Ambulance.java
+++ src/Ambulance.java
@@ -1,4 +1,9 @@
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class Ambulance {
+
     public Ambulance(){
 
     }
