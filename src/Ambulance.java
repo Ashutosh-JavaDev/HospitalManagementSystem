1c4ac5a649818f2965597a63e1e48ac1097d8a72
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-23 22:47:55 +0530

    Hospital Management System

--- src/Ambulance.java
+++ src/Ambulance.java
@@ -2,10 +2,13 @@ import javax.swing.*;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class Ambulance {
+public class Ambulance extends JFrame implements ActionListener{
 
     public Ambulance(){
 
+    }
+    public void actionPerformed(ActionEvent ae){
+        
     }
     public static void main(String[]args){
         new Ambulance() ;
