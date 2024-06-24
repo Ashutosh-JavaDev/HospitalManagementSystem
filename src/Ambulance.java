4e407fade2eef827e477cf4681e1e45fc07b4e81
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-24 21:51:37 +0530

    Hospital Management System

--- src/Ambulance.java
+++ src/Ambulance.java
@@ -10,7 +10,7 @@ public class Ambulance extends JFrame implements ActionListener{
         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
         Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
-        
+        JLabel image=new JLabel(i3);
     }
     public void actionPerformed(ActionEvent ae){
 
