ad072536eedd41710f0de6ce61f8fe80ae4ee24a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-26 11:39:14 +0530

    Hospital Management System

--- src/medical.java
+++ src/medical.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.Color;
 import java.sql.SQLException;
 
@@ -11,6 +12,8 @@ public class medical extends JFrame implements ActionListener {
             immmunizatonfield;
 
     public medical() {
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
+        Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
         // Default
         setLayout(null);
         setTitle("Medical Report");
