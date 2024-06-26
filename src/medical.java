3d828da277a1a4e33894a18444b3d0e73a8987b9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-26 11:23:42 +0530

    Hospital Management System

--- src/medical.java
+++ src/medical.java
@@ -4,16 +4,21 @@ import java.awt.event.ActionListener;
 import java.awt.Font;
 import java.awt.Color;
 import java.sql.SQLException;
-public class medical extends JFrame implements ActionListener{
-    JLabel consulation,DoctorsNotes,diagnostic,treatement,prescription,surgery,immunization;
-    JTextField consultfield,notesfield,diagnosticfield,treatementfield,prescriptionfield,surgeryfield,immmunizatonfield;
-    public medical(){
+
+public class medical extends JFrame implements ActionListener {
+    JLabel consulation, DoctorsNotes, diagnostic, treatement, prescription, surgery, immunization;
+    JTextField consultfield, notesfield, diagnosticfield, treatementfield, prescriptionfield, surgeryfield,
+            immmunizatonfield;
+
+    public medical() {
 
     }
-    public void actionPerformed(ActionEvent ae){
+
+    public void actionPerformed(ActionEvent ae) {
 
     }
-    public  static void main(String[]args){
+
+    public static void main(String[] args) {
         new medical();
     }
 }
