fe5676aa16fcbcd7a3d74a1638ab70781eaf53e9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-26 11:56:36 +0530

    Hospital Management System

--- src/medical.java
+++ src/medical.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+
+import com.toedter.calendar.JDateChooser;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Font;
@@ -10,7 +13,7 @@ public class medical extends JFrame implements ActionListener {
     JLabel consulation, DoctorsNotes, diagnostic, treatement, prescription, surgery, immunization;
     JTextField consultfield, notesfield, diagnosticfield, treatementfield, prescriptionfield, surgeryfield,
             immmunizatonfield;
-
+    JDateChooser consult;
     public medical() {
         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
         Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
@@ -18,6 +21,15 @@ public class medical extends JFrame implements ActionListener {
         JLabel image=new JLabel(i3);
         image.setBounds(0,0,800,600);
         add(image);
+        JLabel head=new JLabel("Medical Report");
+        head.setBounds(300,20,100,40);
+        head.setFont(new Font("Arial",Font.BOLD,22));
+        image.add(head);
+        // consultatiion
+        consulation=new JLabel("Consultation Date:");
+        consulation.setFont(new Font("Arial",Font.BOLD,18));
+        consulation.setBounds(35,75,200,30);
+        image.add(consulation);
         // Default
         setLayout(null);
         setTitle("Medical Report");
