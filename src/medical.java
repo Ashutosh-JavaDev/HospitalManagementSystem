e960dbb3688a9b05ec387cd15d261528171c5ec2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-28 01:38:05 +0530

    Hospital Management System

--- src/medical.java
+++ src/medical.java
@@ -18,10 +18,10 @@ public class medical extends JFrame implements ActionListener {
 
     public medical() {
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(1200, 1000, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
-        image.setBounds(0, 0, 800, 600);
+        image.setBounds(0, 0, 1200, 1000);
         add(image);
         JLabel head = new JLabel("Medical Report");
         head.setBounds(300, 20, 200, 40);
@@ -87,7 +87,7 @@ public class medical extends JFrame implements ActionListener {
         // Default
         setLayout(null);
         setTitle("Medical Report");
-        setSize(800, 600);
+        setSize(1200, 1000);
         setLocation(250, 150);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setUndecorated(true);
