e4a9f45d9563289b1679b8b0e2353c4caaad4551
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-27 23:23:36 +0530

    Hospital Management System

--- src/medical.java
+++ src/medical.java
@@ -14,31 +14,37 @@ public class medical extends JFrame implements ActionListener {
     JTextField notesfield, diagnosticfield, treatementfield, prescriptionfield, surgeryfield,
             immmunizatonfield;
     JDateChooser consult;
+
     public medical() {
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
-        Image i2=i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
-        ImageIcon i3=new ImageIcon(i2);
-        JLabel image=new JLabel(i3);
-        image.setBounds(0,0,800,600);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 800, 600);
         add(image);
-        JLabel head=new JLabel("Medical Report");
-        head.setBounds(300,20,200,40);
-        head.setFont(new Font("Arial",Font.BOLD,22));
+        JLabel head = new JLabel("Medical Report");
+        head.setBounds(300, 20, 200, 40);
+        head.setFont(new Font("Arial", Font.BOLD, 22));
         image.add(head);
         // consultatiion
-        consulation=new JLabel("Consultation Date:");
-        consulation.setFont(new Font("Arial",Font.BOLD,18));
-        consulation.setBounds(35,75,200,30);
+        consulation = new JLabel("Consultation Date:");
+        consulation.setFont(new Font("Arial", Font.BOLD, 18));
+        consulation.setBounds(35, 75, 200, 30);
         image.add(consulation);
         // Consult field
-        consult=new JDateChooser();
-        consult.setBounds(270,75,200,30);
+        consult = new JDateChooser();
+        consult.setBounds(270, 75, 200, 30);
         image.add(consult);
+        // Notes
+        DoctorsNotes = new JLabel("Doctor Notes:");
+        DoctorsNotes.setBounds(300, 20, 200, 40);
+        DoctorsNotes.setFont(new Font("Arial", Font.BOLD, 22));
+        image.add(DoctorsNotes);
         // Default
         setLayout(null);
         setTitle("Medical Report");
-        setSize(800,600);
-        setLocation(250,150);
+        setSize(800, 600);
+        setLocation(250, 150);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setUndecorated(true);
         setVisible(true);
