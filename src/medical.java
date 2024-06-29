11552359206f9cf5c29badbb03aeefa66a2b9c5b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-06-29 21:49:09 +0530

    Hospital Managemennt System

--- src/medical.java
+++ src/medical.java
@@ -140,7 +140,13 @@ public class medical extends JFrame implements ActionListener {
         back.setBackground(Color.black);
         back.addActionListener(this);
         image.add(back);
-
+        // Button
+        confirm=new JButton("Confirm");
+        confirm.setBounds(300,480,100,30);
+        confirm.setFont(new Font("Arial",Font.BOLD,18));
+        confirm.setBackground(Color.black);
+        confirm.addActionListener(this);
+        image.add(confirm);
 
         // Default
         setLayout(null);
