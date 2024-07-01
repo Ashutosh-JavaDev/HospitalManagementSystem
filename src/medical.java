c7dc5d5413e681668fed4c185ef9cc25ec2a05f3
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-01 20:59:45 +0530

    Hospital Managemennt System

--- src/medical.java
+++ src/medical.java
@@ -22,10 +22,10 @@ public class medical extends JFrame implements ActionListener {
 
     public medical() {
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(1000, 800, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
-        image.setBounds(0, 0, 800, 600);
+        image.setBounds(0, 0, 1000, 800);
         add(image);
         JLabel head = new JLabel("Medical Report");
         head.setBounds(300, 10, 600, 60);
@@ -148,7 +148,7 @@ public class medical extends JFrame implements ActionListener {
         image.add(box);
         // Button
         back = new JButton("Back");
-        back.setBounds(35, 600, 100, 30);
+        back.setBounds(300, 600, 100, 30);
         back.setFont(new Font("Arial", Font.BOLD, 18));
         back.setForeground(Color.white);
         back.setBackground(Color.black);
@@ -156,7 +156,7 @@ public class medical extends JFrame implements ActionListener {
         image.add(back);
         // Button
         confirm = new JButton("Confirm");
-        confirm.setBounds(300, 600, 150, 30);
+        confirm.setBounds(500, 600, 150, 30);
         confirm.setFont(new Font("Arial", Font.BOLD, 18));
         confirm.setForeground(Color.white);
         confirm.setBackground(Color.black);
@@ -166,7 +166,7 @@ public class medical extends JFrame implements ActionListener {
         // Default
         setLayout(null);
         setTitle("Medical Report");
-        setSize(800, 600);
+        setSize(1000, 800);
         setLocation(250, 150);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setUndecorated(true);
