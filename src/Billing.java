3eb46a77f984896f4bb03c92e96e5654edb0b3b1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-04 11:20:14 +0530

    Hospital Managemennt System

--- src/Billing.java
+++ src/Billing.java
@@ -4,8 +4,9 @@ import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class Billing{
-    public Billing(){
+
+public class Billing {
+    public Billing() {
 
     }
 }
