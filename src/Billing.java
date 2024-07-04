013c4eef28988a9321849b50b2cf0ff373abb3ae
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-04 11:37:06 +0530

    Hospital Managemennt System

--- src/Billing.java
+++ src/Billing.java
@@ -5,7 +5,7 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Billing {
+public class Billing extends JFrame {
     JLabel patientID,patientName,admissionDate,dischargeDate,roomCharges,consultationFees,medicianFees,labtestCjharges,surgeryCharges,miscellaneousCharges,totalAmount,amountPaid,balanceDue,paymentMethod,billingDate,billingID,attendingPhysician;
     public Billing() {
 
