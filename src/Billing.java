ccb87f7791a18fa5cb80ac99394e928c40d94475
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-05 22:59:44 +0530

    Hospital Managemennt System

--- src/Billing.java
+++ src/Billing.java
@@ -7,18 +7,25 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Billing extends JFrame implements ActionListener{
-    JLabel patientID,patientName,admissionDate,dischargeDate,roomCharges,consultationFees,medicianFees,labtestCharges,surgeryCharges,miscellaneousCharges,totalAmount,amountPaid,balanceDue,paymentMethod,billingDate,billingID,attendingPhysician;
-    JTextField idfield,namefield,roomfield,consultfield,feefield,labfield,surgeryfield,miscellaneousfield,totalfield,amountfield,balancefield,billingfield,physicianfield;
-    JDateChooser addmissionfield,dischargefield;
+public class Billing extends JFrame implements ActionListener {
+    JLabel patientID, patientName, admissionDate, dischargeDate, roomCharges, consultationFees, medicianFees,
+            labtestCharges, surgeryCharges, miscellaneousCharges, totalAmount, amountPaid, balanceDue, paymentMethod,
+            billingDate, billingID, attendingPhysician;
+    JTextField idfield, namefield, roomfield, consultfield, feefield, labfield, surgeryfield, miscellaneousfield,
+            totalfield, amountfield, balancefield, billingfield, physicianfield;
+    JDateChooser addmissionfield, dischargefield;
     JRadioButton methodfield;
+
     public Billing() {
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
-        Image i2=i1.getImage().getScaledInstance(1000, 800, Image.SCALE_DEFAULT);
-        ImageIcon i3=new ImageIcon(i2);
-        JLabel image=new JLabel(i3);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1000, 800, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 1000, 800);
+        add(image);
     }
-    public void actionPerformed(ActionEvent ae){
+
+    public void actionPerformed(ActionEvent ae) {
 
     }
 }
