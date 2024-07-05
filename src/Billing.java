e112bf01b04f782f322f163daa4d5eed4786be60
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-05 23:01:39 +0530

    Hospital Managemennt System

--- src/Billing.java
+++ src/Billing.java
@@ -34,4 +34,7 @@ public class Billing extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent ae) {
 
     }
+    public static void main(String[]args){
+        new Billing();
+    }
 }
