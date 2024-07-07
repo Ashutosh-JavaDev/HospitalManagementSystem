ef1c3ee9e667342372f3065c2fab6795f016db0f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-07 22:17:09 +0530

    Hospital Managemennt System

--- src/Billing.java
+++ src/Billing.java
@@ -69,6 +69,14 @@ public class Billing extends JFrame implements ActionListener {
         feefield=new JTextField();
         feefield.setBounds(180,230,200,30);
         image.add(feefield);
+        // 
+        labtestCharges=new JLabel("Lab Charges");
+        labtestCharges.setBounds(30,270,200,30);
+        labtestCharges.setFont(new Font("Arial",Font.BOLD,16));
+        image.add(labtestCharges);
+        idfield=new JTextField();
+        idfield.setBounds(180,70,200,30);
+        image.add(idfield);
         // Button
         back = new JButton("Back");
         back.setBounds(800, 650, 100, 30);
