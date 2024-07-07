7a5bcf0577425907873d3efeab0c22e46a62d4ff
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-07 21:47:41 +0530

    Hospital Managemennt System

--- src/Billing.java
+++ src/Billing.java
@@ -15,7 +15,7 @@ public class Billing extends JFrame implements ActionListener {
     JTextField idfield, namefield, roomfield, consultfield, feefield, labfield, surgeryfield, miscellaneousfield,
             totalfield, amountfield, balancefield, billingfield, physicianfield;
     JDateChooser addmissionfield, dischargefield;
-    JButton confirm,back;
+    JButton confirm, back;
     JRadioButton methodfield;
 
     public Billing() {
@@ -25,41 +25,50 @@ public class Billing extends JFrame implements ActionListener {
         JLabel image = new JLabel(i3);
         image.setBounds(0, 0, 1000, 800);
         add(image);
-        JLabel title=new JLabel("Billing");
-        title.setBounds(450,10,180,45);
-        title.setFont(new Font("Arial",Font.BOLD,35));
+        JLabel title = new JLabel("Billing");
+        title.setBounds(450, 10, 180, 45);
+        title.setFont(new Font("Arial", Font.BOLD, 35));
         image.add(title);
-        patientID=new JLabel("Patient ID");
-        patientID.setBounds(30,70,200,30);
-        patientID.setFont(new Font("Arial",Font.BOLD,16));
+        //
+        patientID = new JLabel("Patient ID");
+        patientID.setBounds(30, 70, 200, 30);
+        patientID.setFont(new Font("Arial", Font.BOLD, 16));
         image.add(patientID);
-        idfield=new JTextField();
-        idfield.setBounds(180,70,200,30);
+        idfield = new JTextField();
+        idfield.setBounds(180, 70, 200, 30);
         image.add(idfield);
-
+        //
+        patientName = new JLabel("Patient Name");
+        patientName.setBounds(30, 70, 200, 30);
+        patientName.setFont(new Font("Arial", Font.BOLD, 16));
+        image.add(patientName);
+        idfield = new JTextField();
+        idfield.setBounds(180, 70, 200, 30);
+        image.add(idfield);
         // Button
-        back=new JButton("Back");
-        back.setBounds(800,650,100,30);
+        back = new JButton("Back");
+        back.setBounds(800, 650, 100, 30);
         back.setBackground(Color.BLACK);
         back.setForeground(Color.white);
-        back.setFont(new Font("Arial",Font.BOLD,20));
+        back.setFont(new Font("Arial", Font.BOLD, 20));
         back.addActionListener(this);
         image.add(back);
         // Default
-        setSize(1000,800);
+        setSize(1000, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocation(250,150);
+        setLocation(250, 150);
         setUndecorated(true);
         setLayout(null);
         setVisible(true);
     }
 
     public void actionPerformed(ActionEvent ae) {
-        if(ae.getSource()==back){
+        if (ae.getSource() == back) {
             System.exit(0);
         }
     }
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         new Billing();
     }
 }
