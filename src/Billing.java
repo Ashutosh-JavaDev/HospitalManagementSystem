1805bc413e7ac19a483b3c28014549d6f1b39bfe
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-09 22:35:02 +0530

    Hospital Managemennt System

--- src/Billing.java
+++ src/Billing.java
@@ -20,7 +20,7 @@ public class Billing extends JFrame implements ActionListener {
 
     public Billing() {
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/pexels-scottwebb-311458.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(1000, 600, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
         image.setBounds(0, 0, 1000, 600);
