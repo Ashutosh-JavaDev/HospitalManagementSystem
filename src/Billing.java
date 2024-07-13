e5bf8b034a51d8cbf749f01f2b3097d635feccdb
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-13 14:27:49 +0530

    Hospital Management System

--- src/Billing.java
+++ src/Billing.java
@@ -200,6 +200,7 @@ public class Billing extends JFrame implements ActionListener {
             String Billingfield = billingfield.getText();
             String Physicianfield = physicianfield.getText();
             String Addmissionfield=((JTextField)addmissionfield.getDateEditor().getUiComponent()).getText();
+            String Dischargefield=((JTextField)dischargefield.getDateEditor().getUiComponent()).getText();
         }
     }
 
