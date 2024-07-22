6982e1b0a98df0b7dfb7b2dd1dc4facfc2bfeafc
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-22 20:50:17 +0530

    Afficiation

--- src/Billing.java
+++ src/Billing.java
@@ -205,7 +205,7 @@ public class Billing extends JFrame implements ActionListener {
             String Method=(String)methodfield.getSelectedItem();
             try{
                 HospitalManagementSystem conn=new HospitalManagementSystem();
-                String query="insert into billing values()";
+                String query="insert into billing values('"+IDfield+"')";
             }
             catch(SQLException a){
                 a.printStackTrace();
